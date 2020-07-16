80a814a0ebb80a29e2b70ecf0c1a18518b15911c
ssbeast <ssbeast007@gmail.com>
2020-07-16 18:29:42 +0530

    minor changes

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -59,7 +59,7 @@ class StoryBrain {
       _storyNumber = 5;
     } else if (choiceNumber == 2 && _storyNumber == 2) {
       _storyNumber = 4;
-    } else if (choiceNumber == 3 || choiceNumber == 4 || choiceNumber == 5) {
+    } else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
       restart();
     }
   }
