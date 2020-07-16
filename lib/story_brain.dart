32d83de64ecc0f07713abcdc02f0e9498fa43ca7
ssbeast <ssbeast007@gmail.com>
2020-07-16 18:01:47 +0530

    story restarted

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -63,10 +63,14 @@ class StoryBrain {
       storyNumber = 5;
     } else if (choiceNumber == 2 && storyNumber == 2) {
       storyNumber = 4;
+    } else if (choiceNumber == 3 || choiceNumber == 4 || choiceNumber == 5) {
+      restart();
     }
   }
-}
 
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
+  void restart() {
+    storyNumber = 0;
+  }
+}
 
 //TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
